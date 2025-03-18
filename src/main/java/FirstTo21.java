365ab5a0ad6d620f3f2a962721ff0e6c05f6fea1
kimol <kim.olofssonn@outlook.com>
2025-03-18 10:38:28 +0100

    uppdaterat metoder för vem som börjar, dator väljer på slump tydligare beskrivningar

--- src/main/java/FirstTo21.java
+++ src/main/java/FirstTo21.java
@@ -1,34 +1,44 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class FirstTo21 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         int currentNumber = 0;
 
-        System.out.println("Välkommen till spelet! Den som säger 21 vinner.");
-        System.out.println("Du börjar!");
-
+        System.out.println("""
+                Välkommen till spelet! Den som säger 21 vinner.
+                Vi singlar slant om vem som börjar du är krona!
+                """);
+        int playerMove = random.nextInt(2) + 1;
+        if (playerMove == 1) {
+            System.out.println("Krona du börjar!");
+        } else {
+            System.out.println("klave datorn börjar!");
+        }
         while (true) {
-            int playerMove = 0;
-            while (playerMove != 1 && playerMove != 2) {
-                System.out.print("Säg 1 eller 2: ");
-                playerMove = scanner.nextInt();
-                if (playerMove != 1 && playerMove != 2) {
+            if (playerMove == 1) {
+                System.out.print("Lägg till 1 eller 2: ");
+                int move = scanner.nextInt();
+                while (move != 1 && move != 2) {
                     System.out.println("Ogiltigt val, försök igen.");
+                    System.out.print("Lägg till 1 eller 2: ");
+                    move = scanner.nextInt();
+                }
+                currentNumber += move;
+                System.out.println("Du sa: " + currentNumber);
+
+                if (currentNumber >= 21) {
+                    System.out.println("Grattis! Du vinner!");
+                    break;
                 }
             }
 
-            currentNumber += playerMove;
-            System.out.println("Du sa: " + currentNumber);
-
-            if (currentNumber >= 21) {
-                System.out.println("Grattis! Du vinner!");
-                break;
-            }
-
-            int computerMove = ((currentNumber+1) % 3) == 0 ? 1 : 2;
+            int computerMove = random.nextInt(2) + 1;
             currentNumber += computerMove;
-            System.out.println("Datorn säger: " + currentNumber);
+            System.out.println("Datorn väljer: " + computerMove);
+            System.out.println("Ställningen är: " + currentNumber);
 
             if (currentNumber >= 21) {
                 System.out.println("Datorn vinner! Bättre lycka nästa gång.");
