f2f1c5929a58c29088c4e7e7091aa4bcbdd81ba7
malindqvist <malindqvist@outlook.com>
2025-03-18 10:36:22 +0100

    Malins fix för att ändra till ett dynamiskt nummer.

--- src/main/java/FirstTo21.java
+++ src/main/java/FirstTo21.java
@@ -4,22 +4,27 @@ public class FirstTo21 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int currentNumber = 0;
+        int choice1 = 1;
+        int choice2 = 2;
 
         System.out.println("Välkommen till spelet! Den som säger 21 vinner.");
         System.out.println("Du börjar!");
 
+
         while (true) {
             int playerMove = 0;
-            while (playerMove != 1 && playerMove != 2) {
-                System.out.print("Säg 1 eller 2: ");
+            while (playerMove != choice1 && playerMove != choice2) {
+                System.out.print("Säg " + choice1 + " eller " + choice2 + ": ");
                 playerMove = scanner.nextInt();
-                if (playerMove != 1 && playerMove != 2) {
+                if (playerMove != choice1 && playerMove != choice2) {
                     System.out.println("Ogiltigt val, försök igen.");
                 }
             }
 
             currentNumber += playerMove;
             System.out.println("Du sa: " + currentNumber);
+            choice1 += 2;
+            choice2 += 2;
 
             if (currentNumber >= 21) {
                 System.out.println("Grattis! Du vinner!");
