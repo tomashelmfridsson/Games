f139a9f537afba14f576bd96c6db7e23fc03ac3b
kimol <kim.olofssonn@outlook.com>
2025-03-18 11:01:14 +0100

    fixar bugg där datorn får köra tills han vinner om han börjar. lägger till spelpjäser.

--- src/main/java/FirstTo21.java
+++ src/main/java/FirstTo21.java
@@ -7,43 +7,50 @@ public class FirstTo21 {
         Random random = new Random();
         int currentNumber = 0;
 
+            //spelpjäser
+        String red = "\u001B[31m";
+        String green = "\u001B[32m";
+        String reset = "\u001B[0m";
+
         System.out.println("""
                 Välkommen till spelet! Den som säger 21 vinner.
                 Vi singlar slant om vem som börjar du är krona!
                 """);
         int playerMove = random.nextInt(2) + 1;
         if (playerMove == 1) {
-            System.out.println("Krona du börjar!");
+            System.out.println("Krona du börjar!"+reset);
         } else {
-            System.out.println("klave datorn börjar!");
+            System.out.println("Klave datorn börjar!"+reset);
         }
         while (true) {
             if (playerMove == 1) {
-                System.out.print("Lägg till 1 eller 2: ");
+                System.out.print("Lägg till 1 eller 2: "+reset);
                 int move = scanner.nextInt();
                 while (move != 1 && move != 2) {
-                    System.out.println("Ogiltigt val, försök igen.");
-                    System.out.print("Lägg till 1 eller 2: ");
+                    System.out.println("Ogiltigt val, försök igen."+reset);
+                    System.out.print("Lägg till 1 eller 2: "+reset);
                     move = scanner.nextInt();
                 }
                 currentNumber += move;
-                System.out.println("Du sa: " + currentNumber);
+                System.out.println(green +"Du sa: " + move +reset);
+                System.out.println("Ställningen är: " + currentNumber);
 
                 if (currentNumber >= 21) {
-                    System.out.println("Grattis! Du vinner!");
+                    System.out.println(green +"Grattis! Du vinner!"+reset);
                     break;
                 }
             }
-
             int computerMove = random.nextInt(2) + 1;
             currentNumber += computerMove;
-            System.out.println("Datorn väljer: " + computerMove);
+            System.out.println(red + "Datorn väljer: " + computerMove + reset);
             System.out.println("Ställningen är: " + currentNumber);
 
             if (currentNumber >= 21) {
-                System.out.println("Datorn vinner! Bättre lycka nästa gång.");
+                System.out.println(red +"Datorn vinner! Bättre lycka nästa gång." + reset);
                 break;
             }
+            playerMove=1;
+
         }
         scanner.close();
     }
